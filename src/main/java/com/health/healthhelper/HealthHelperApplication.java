68ce7f430923f7a91fc18771f6475beba452731f
Lucas <sht4873@nate.com>
2024-01-23 00:16:13 +0900

    create health-helper

---
+++ src/main/java/com/health/healthhelper/HealthHelperApplication.java
@@ -0,0 +1,13 @@
+package com.health.healthhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthHelperApplication.class, args);
+    }
+
+}
